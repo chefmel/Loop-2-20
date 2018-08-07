c8ea41c7a4d44ad281c841b155ea65e5ed67467b
Melissa DoddRuesing <melissa.doddruesing@gmail.com>
2018-08-07 18:45:20 -0500

    CG 4-1

--- Loop 2-20/Program.cs
+++ Loop 2-20/Program.cs
@@ -6,7 +6,18 @@ namespace Loop_2_20
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a number between 1 and 10");
+            int x = int.Parse(Console.ReadLine());
+            var sum = 0;
+
+            for (int i = 2; i < 21; i++)
+            {
+                Console.WriteLine(i * x);
+                sum = sum + i * x;
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
         }
+
     }
 }
